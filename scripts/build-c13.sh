e50a8b7762c9a7c58e129dd3477a17aa8d28aae2
lwz114 <439005140@qq.com>
2026-08-21 10:30:27 +0800

    fix c13 toolchain

--- scripts/build-c13.sh
+++ scripts/build-c13.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabihf-
 OUT=out-c13
 make O="$OUT" readboy_c13_defconfig
 make O="$OUT" -j"$(nproc)" Image.gz dtbs
